0da1404bf9fe5c86c836764298f650ac9b3e5a81
DefPavel <33838515+DefPavel@users.noreply.github.com>
2022-02-01 15:47:47 +0300

    add dockerfile

--- Startup.cs
+++ Startup.cs
@@ -30,12 +30,13 @@ namespace sync_swagger
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "sync_swagger v1"));
-            }
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "sync_swagger v1"));
+
             app.UseRouting();
 
             app.UseAuthorization();
